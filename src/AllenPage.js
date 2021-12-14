799edcd177aebf5c8755b8de1acd90fd46bf19f1
Chris Cha <chrisjwc@uw.edu>
2021-12-13 20:52:19 -0800

    Added Multiple Library Pages

--- src/AllenPage.js
+++ src/AllenPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function LibInfo() {
+export function Allen() {
     return (
         <div>
             <div>
--- src/Navigation.js
+++ src/Navigation.js
@@ -4,25 +4,39 @@ export  function Navigation () {
     return(
         <header className="welcome">
 
-        <h1>Welcome to UW Library Directory!</h1>
-        <nav className="top-nav">
-            <div className="top-nav-content">
-                <a href="/">Home </a>
-            </div>
+            <h1>Welcome to UW Library Directory!</h1>
+                <nav className="top-nav">
+                    <div className="top-nav-content">
+                        <a href="/">Home </a>
+                    </div>
 
-            <div className="top-nav-content">
-                <a href="/lib_info"> Library Info</a>
-            </div>
+                    <div className="top-nav-content">
+                        <div class="dropdown">
+                            <button class="dropbtn">Library Info
+                                <i class="fa fa-caret-down"></i>
+                            </button>
+                            <div class="dropdown-content">
+                                <a href="/ArtsPage">Art Library</a>
+                                <a href="/AllenPage">Allen Library</a>
+                                <a href="/BuiltEnvirPage">Built Environment</a>
+                                <a href="/EastAsiaPage">East Asia Library</a>
+                                <a href="/EngineeringPage">Engineering Library</a>
+                                <a href="/FosterBuisnessPage">Foster Buisness Library</a>
+                                <a href="/SuzzaloPage">Suzzalo Library</a>
+                                <a href="/OdegaardUnderPage">Odegaard Undergraduate Library</a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="top-nav-content">
+                        <a href="/filter">Filter</a>
+                    </div>
 
-            <div className="top-nav-content">
-                <a href="/filter">Filter</a>
-            </div>
+                    <div className="top-nav-content">
+                        <a href="/review">Reviews</a>
+                    </div>
 
-            <div className="top-nav-content">
-                <a href="/review">Reviews</a>
-            </div>
-
-        </nav>
-    </header>
+                </nav>
+        </header>
     )
 }
